cbad35d9874927395a8fa701a05846c0c1c0d6d3
Gugo-le <guhyunseung0718@gmail.com>
2022-11-07 07:51:54 +0900

    update

---
+++ 조건문/if-else.py
@@ -0,0 +1,28 @@
+money = 1500
+
+if money < 10:
+    pass
+
+else:                               # 들여쓰기 중요
+    print("저금해야겠군..")
+    
+    
+a = 3   
+b = 5
+if a > b:
+    print(a)
+else:
+    print(b)    
+    
+c = 15 * 5
+d = 15 + 15 + 15 + 15 + 15
+if c > d:
+    print('c is greater than d')
+elif c == d:
+     print('c is equal to d')
+elif c < d:
+    print('c is less than d')
+else:
+    print('I don\'t know')    
+    
+    # 조건을 여러번 줄 수도 있다.
